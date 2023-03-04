9f0cd7cc1d432377441ec46a077a88bc13db98e4
Simon Wiles <simonjwiles@gmail.com>
2023-03-03 23:32:27 -0800

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+// https://github.com/withastro/prettier-plugin-astro#pnpm-support-1
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
