9f1760e23bb3d3ccd9de14296bb3e7cd5b03c862
Simon Wiles <simonjwiles@gmail.com>
2023-06-01 18:44:48 -0700

    Set `scopedStyleStrategy: 'class'`
    Default CSS behaviour appears to have changed; setting `scopedStyleStrategy: 'class'` ensures component styles continue to override global styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { ogImageGenerator } from "./integrations/og-image-generator.ts";
 export default defineConfig({
   site: "https://hsiaolin.net/",
   trailingSlash: "always",
+  scopedStyleStrategy: 'class',
   integrations: [
     mdx(),
     sitemap(),
